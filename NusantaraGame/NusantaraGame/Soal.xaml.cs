a9634ded294dd84d41b5b2c7d4494b62306c8db3
Uswatun Hasana Kunio <kunio.nurul@gmail.com>
2014-06-14 12:36:55 +0700

    Perubahan di Halaman Level dan Soal, diberikan parameter

---
+++ NusantaraGame/NusantaraGame/Soal.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NusantaraGame
+{
+    /// <summary>
+    /// Interaction logic for Soal.xaml
+    /// </summary>
+    public partial class Soal : Window
+    {
+        public Soal()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowDialog(ref TextBlock TextLevel)
+        {
+            TextSoal.Text = TextLevel.Text;
+
+            this.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
