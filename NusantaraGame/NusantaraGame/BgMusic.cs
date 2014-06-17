c10752641aa7266b0fc2b2a80cc14e5e78e34dcc
Uswatun Hasana Kunio <kunio.nurul@gmail.com>
2014-06-17 07:10:57 +0700

    Menambahkan kelas BgMusic.cs dan musik desaku.wav

---
+++ NusantaraGame/NusantaraGame/BgMusic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NusantaraGame
+{
+    class BgMusic
+    {
+        public string BgMusicPath{get;set;}
+    }
+}
